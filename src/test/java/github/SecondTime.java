08b6ee76ec9115a8220bacebe1e24a4b421e7c0b
Noire <noireflynn@gmail.com>
2023-02-07 22:34:12 +0400

    New

--- src/test/java/github/SecondTime.java
+++ src/test/java/github/SecondTime.java
@@ -1,12 +1,16 @@
 package github;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SecondTime {
@@ -17,6 +21,9 @@ public class SecondTime {
         open("https://github.com/");
 $(".js-site-search-focus").setValue("Selenide").pressEnter();
 $$(".repo-list li").first().$("a").click();
-
+$(".Layout-sidebar").$(Selectors.byText("contributors"))
+.ancestor(".BorderGrid-cell").$$("ul li")
+        .first().hover();
+        $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
     }
 }
