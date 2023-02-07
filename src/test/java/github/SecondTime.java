7f7952cdd9003c74ff53e13b28052e1171b9deb1
Noire <noireflynn@gmail.com>
2023-02-07 22:01:58 +0400

    New

--- src/test/java/github/SecondTime.java
+++ src/test/java/github/SecondTime.java
@@ -7,18 +7,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SecondTime {
     @Test
     void secondTime(){
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 400000;
-        open("http://193.47.154.27:9944/lite/");
-$("#br_login").setValue("000000061");
-$("#br_pwd").setValue("A").pressEnter();
+        open("https://github.com/");
+$(".js-site-search-focus").setValue("Selenide").pressEnter();
+$$(".repo-list li").first().$("a").click();
 
     }
 }
